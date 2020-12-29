c2e8355dbc8932c63bcebe2d4ee45a6d221087ce
Nesya <angerasimov04@outlook.com>
2020-12-29 15:50:17 +0300

    Добавлена 1 атака противника Добавлено окно GameOver (для себя, потом удалю)

--- fighting.py
+++ fighting.py
@@ -1,13 +1,16 @@
 import pygame
+import time
 
 FPS = 60
-horizontal_border_size = 600
+horizontal_border_size = 350
 horizontal_borders_sprites = pygame.sprite.Group()
 
-vertical_border_size = 300
+vertical_border_size = 350
 vertical_borders_sprites = pygame.sprite.Group()
 
 player_sprite = pygame.sprite.Group()
+blinking_sprites = pygame.sprite.Group()
+killer_sprites = pygame.sprite.Group()
 
 
 class Player(pygame.sprite.Sprite):
@@ -16,6 +19,7 @@ class Player(pygame.sprite.Sprite):
         self.width = 25
         self.height = 25
         self.speed = speed
+        self.islive = True
         self.image = pygame.Surface((self.width, self.width),
                                     pygame.SRCALPHA, 32)
         pygame.draw.rect(self.image, pygame.Color('blue'),
@@ -23,6 +27,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, self.width, self.width)
 
     def move(self, dx, dy):
+        if not self.islive:
+            return
         if pygame.sprite.spritecollideany(self, vertical_borders_sprites) or \
                 pygame.sprite.spritecollideany(self, horizontal_borders_sprites):
             return
@@ -33,28 +39,74 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= dx
             self.rect.y -= dy
 
+    def update(self):
+        if not self.islive:
+            return
+        if pygame.sprite.spritecollideany(self, killer_sprites):
+            self.islive = False
+            return
+
 
 class Border(pygame.sprite.Sprite):
     def __init__(self, x1, y1, x2, y2):
         if x1 == x2:
             super().__init__(vertical_borders_sprites)
-            self.image = pygame.Surface([1, y2 - y1])
+            self.image = pygame.Surface((1, y2 - y1))
             pygame.draw.rect(self.image, pygame.Color('white'),
                              (0, 0, 1, y2 - y1))
             self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
         else:
             super().__init__(horizontal_borders_sprites)
-            self.image = pygame.Surface([x2 - x1, 1])
+            self.image = pygame.Surface((x2 - x1, 1))
             pygame.draw.rect(self.image, pygame.Color('white'),
                              (0, 0, x2 - x1, 1))
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
+class Killer(pygame.sprite.Sprite):
+    def __init__(self, rect):
+        super().__init__(killer_sprites)
+        self.image = pygame.Surface((rect.w, rect.h), pygame.SRCALPHA, 32)
+        pygame.draw.rect(self.image, pygame.Color('white'),
+                         (0, 0, rect.w, rect.h))
+        self.rect = pygame.Rect(rect)
+
+
+class Blinking(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, w, h):
+        super().__init__(blinking_sprites)
+        self.current_time = pygame.time.get_ticks()
+        self.iswhite = True
+        self.image = pygame.Surface((w, h), pygame.SRCALPHA, 32)
+        self.count_of_blinking = 0
+        pygame.draw.rect(self.image, pygame.Color('white'),
+                         (0, 0, w, h))
+        self.rect = pygame.Rect(x1, y1, w, h)
+
+    def update(self):
+        if self.count_of_blinking == 8:
+            blinking_sprites.remove(self)
+            Killer(self.rect)
+        current_time = pygame.time.get_ticks()
+
+        # Время мигания
+        delay = 150
+
+        if current_time >= self.current_time + delay:
+            self.iswhite = not self.iswhite
+            if self.iswhite:
+                self.image.fill(pygame.Color('white'))
+            else:
+                self.image.fill(pygame.Color('black'))
+            self.current_time = current_time
+            self.count_of_blinking += 1
+
+
 def fighting():
-    pygame.init()
     width, height = 1600, 900
     screen = pygame.display.set_mode((width, height))
 
+    #  attack = random.choice([0, 1, 2, 3, 4, 5])
     border_left_x = (width / 2 - horizontal_border_size / 2) - 1
     border_top_y = (height / 2 + 50) - 1
     border_right_x = border_left_x + horizontal_border_size + 1
@@ -90,6 +142,9 @@ def fighting():
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_0:
+                    Blinking(border_left_x + 1, border_top_y + 1, 100, 350)
+                    Blinking(border_right_x - 100, border_top_y + 1, 100, 350)
                 if event.key == pygame.K_LEFT:
                     pressed_keys["left"] = True
                 if event.key == pygame.K_RIGHT:
@@ -118,12 +173,15 @@ def fighting():
         if pressed_keys["down"]:
             player.move(0, tomove)
 
+        if not player.islive:
+            return
         screen.fill(pygame.Color('black'))
+        blinking_sprites.update()
         player_sprite.update()
         vertical_borders_sprites.draw(screen)
         horizontal_borders_sprites.draw(screen)
+        blinking_sprites.draw(screen)
+        killer_sprites.draw(screen)
         player_sprite.draw(screen)
         clock.tick(FPS)
         pygame.display.flip()
-
-    pygame.quit()
--- mymain.py
+++ mymain.py
@@ -1,6 +1,11 @@
+import pygame
 from fighting import fighting
+from gameover import over
 import sys
 
 if __name__ == "__main__":
+    pygame.init()
     fighting()
+    over()
+    pygame.quit()
     sys.exit()
